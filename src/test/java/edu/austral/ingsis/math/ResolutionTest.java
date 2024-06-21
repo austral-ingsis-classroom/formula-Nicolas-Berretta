f66ccc39154f465f18cd09d2bc90d4c707402476
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-06-20 23:19:41 -0300

    passing ResolutionTest and ResolutionWithVariablesTest

--- src/test/java/edu/austral/ingsis/math/ResolutionTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionTest.java
@@ -3,6 +3,10 @@ package edu.austral.ingsis.math;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.austral.ingsis.math.operation.Value;
+import edu.austral.ingsis.math.operation.composedOperation.*;
+import edu.austral.ingsis.math.operation.singleOperation.Module;
+import edu.austral.ingsis.math.operation.singleOperation.SquareRoot;
 import org.junit.jupiter.api.Test;
 
 public class ResolutionTest {
@@ -10,64 +14,67 @@ public class ResolutionTest {
   /** Case 1 + 6 */
   @Test
   public void shouldResolveSimpleFunction1() {
-    final Double result = 7d;
-
+    Function function = new Addition(new Value(1.0),new Value(6.0));
+    final Double result = function.solve();
     assertThat(result, equalTo(7d));
   }
 
   /** Case 12 / 2 */
   @Test
   public void shouldResolveSimpleFunction2() {
-    final Double result = 6d;
-
+    Function function = new Division(new Value(12.0),new Value(2.0));
+    final Double result = function.solve();
     assertThat(result, equalTo(6d));
   }
 
   /** Case (9 / 2) * 3 */
   @Test
   public void shouldResolveSimpleFunction3() {
-    final Double result = 13.5;
-
+    Function division = new Division(new Value(9.0),new Value(2.0));
+    Function multiplication = new Multiplication(division,new Value(3.0));
+    final Double result = multiplication.solve();
     assertThat(result, equalTo(13.5d));
   }
 
   /** Case (27 / 6) ^ 2 */
   @Test
   public void shouldResolveSimpleFunction4() {
-    final Double result = 20.25;
-
+    Function division = new Division(new Value(27.0),new Value(6.0));
+    Function power = new Power(division,new Value(2.0));
+    final Double result = power.solve();
     assertThat(result, equalTo(20.25d));
   }
 
   /** Case 36 ^ (1/2) */
   @Test
   public void shouldResolveSimpleFunction5() {
-    final Double result = 6d;
-
+    Function squareRoot = new SquareRoot(new Value(36.0));
+    final Double result = squareRoot.solve();
     assertThat(result, equalTo(6d));
   }
 
   /** Case |136| */
   @Test
   public void shouldResolveSimpleFunction6() {
-    final Double result = 136d;
-
+    Function module = new Module(new Value(136.0));
+    final Double result = module.solve();
     assertThat(result, equalTo(136d));
   }
 
   /** Case |-136| */
   @Test
   public void shouldResolveSimpleFunction7() {
-    final Double result = 136d;
-
+    Function module = new Module(new Value(-136.0));
+    final Double result = module.solve();
     assertThat(result, equalTo(136d));
   }
 
   /** Case (5 - 5) * 8 */
   @Test
   public void shouldResolveSimpleFunction8() {
-    final Double result = 0d;
-
+    Function sub = new Subtraction(new Value(5.0), new Value(5.0));
+    Function mult = new Multiplication(sub, new Value(8.0));
+    final Double result = mult.solve();
     assertThat(result, equalTo(0d));
   }
 }
--- src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionWithVariablesTest.java
@@ -3,6 +3,11 @@ package edu.austral.ingsis.math;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.austral.ingsis.math.operation.Value;
+import edu.austral.ingsis.math.operation.Variable;
+import edu.austral.ingsis.math.operation.composedOperation.*;
+import edu.austral.ingsis.math.operation.singleOperation.Module;
+import edu.austral.ingsis.math.operation.singleOperation.SquareRoot;
 import org.junit.jupiter.api.Test;
 
 public class ResolutionWithVariablesTest {
@@ -10,64 +15,69 @@ public class ResolutionWithVariablesTest {
   /** Case 1 + x where x = 3 */
   @Test
   public void shouldResolveFunction1() {
-    final Double result = 4d;
-
+    Function function = new Addition(new Value(1.0), new Variable("x",new Value(3.0)));
+    final Double result = function.solve();
     assertThat(result, equalTo(4d));
   }
 
   /** Case 12 / div where div = 4 */
   @Test
   public void shouldResolveFunction2() {
-    final Double result = 3d;
-
+    Function function = new Division(new Value(12.0), new Variable("div",new Value(4.0)));
+    final Double result = function.solve();
     assertThat(result, equalTo(3d));
   }
 
   /** Case (9 / x) * y where x = 3 and y = 4 */
   @Test
   public void shouldResolveFunction3() {
-    final Double result = 12d;
-
+    Function division = new Division(new Value(9.0), new Variable("x",new Value(3.0)));
+    Function multiplication = new Multiplication(division, new Variable("y",new Value(4.0)));
+    final Double result = multiplication.solve();
     assertThat(result, equalTo(12d));
   }
 
   /** Case (27 / a) ^ b where a = 9 and b = 3 */
   @Test
   public void shouldResolveFunction4() {
-    final Double result = 27d;
-
+    Function division = new Division(new Value(27.0), new Variable("a",new Value(9.0)));
+    Function power = new Power(division, new Variable("b",new Value(3.0)));
+    final Double result = power.solve();
     assertThat(result, equalTo(27d));
   }
 
   /** Case z ^ (1/2) where z = 36 */
   @Test
   public void shouldResolveFunction5() {
-    final Double result = 6d;
-
+    Function squareRoot = new SquareRoot(new Variable("z", new Value(36.0)));
+    final Double result = squareRoot.solve();
     assertThat(result, equalTo(6d));
   }
 
   /** Case |value| - 8 where value = 8 */
   @Test
   public void shouldResolveFunction6() {
-    final Double result = 0d;
-
+    Function module = new Module(new Variable("value", new Value(8.0)));
+    Function subtraction  = new Subtraction(module, new Value(8.0));
+    final Double result = subtraction.solve();
     assertThat(result, equalTo(0d));
   }
 
   /** Case |value| - 8 where value = 8 */
   @Test
-  public void shouldResolveFunction7() {
-    final Double result = 0d;
-
+  public void shouldResolveFunction7() { //wouldn't be value = -8
+    Function module = new Module(new Variable("value", new Value(-8.0)));
+    Function subtraction  = new Subtraction(module, new Value(8.0));
+    final Double result = subtraction.solve();
     assertThat(result, equalTo(0d));
   }
 
   /** Case (5 - i) * 8 where i = 2 */
   @Test
   public void shouldResolveFunction8() {
-    final Double result = 24d;
-
+    Function subtraction  = new Subtraction(new Value(5.0), new Variable("i",new Value(2.0)));
+    Function multiplication  = new Multiplication(subtraction, new Value(8.0));
+    final Double result = multiplication.solve();
     assertThat(result, equalTo(24d));
   }
 }
